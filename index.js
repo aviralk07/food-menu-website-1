857ed5f1bdc1c50bbdea6346c4d155b7834e39ca
aviral kalra <aviralkalra2104@gmail.com>
2022-09-20 11:18:55 +0530

    init

---
+++ index.js
@@ -0,0 +1,105 @@
+function myFunction() {
+  location.replace("http://127.0.0.1:5500/menu.html")
+}
+
+
+function myFunctio() {
+  location.replace("http://127.0.0.1:5500/soup.html")
+}
+function mylunch() {
+  location.replace("http://127.0.0.1:5500/lunch.html")
+}
+
+
+
+function myappetizer() {
+  location.replace("http://127.0.0.1:5500/appetizer.html")
+}
+
+
+
+
+function mychef() {
+  location.replace("http://127.0.0.1:5500/chef.html")
+}
+
+
+function mychicken() {
+  location.replace("http://127.0.0.1:5500/chicken.html")
+}
+
+
+
+function mybeef() {
+  location.replace("http://127.0.0.1:5500/beef.html")
+}
+
+
+
+function myveal() {
+  location.replace("http://127.0.0.1:5500/veal.html")
+}
+
+
+
+
+function myduck() {
+  location.replace("http://127.0.0.1:5500/duck.html")
+}
+
+
+function myvegetable() {
+  location.replace("http://127.0.0.1:5500/vegetable.html")
+}
+
+
+
+function mycurry() {
+  location.replace("http://127.0.0.1:5500/curry.html")
+}
+
+function mynoddle() {
+  location.replace("http://127.0.0.1:5500/noodle.html")
+}
+
+
+function mymeifan() {
+  location.replace("http://127.0.0.1:5500/meifan.html")
+}
+
+function mypanfry() {
+  location.replace("http://127.0.0.1:5500/panfry.html")
+}
+
+
+function myfriedrice() {
+  location.replace("http://127.0.0.1:5500/friedrice.html")
+}
+
+
+function mychowmein() {
+  location.replace("http://127.0.0.1:5500/chowmein.html")
+}
+
+function myegg() {
+  location.replace("http://127.0.0.1:5500/egg.html")
+}
+
+
+function mysideorder() {
+  location.replace("http://127.0.0.1:5500/sideorder.html")
+}
+
+function mydessert() {
+  location.replace("http://127.0.0.1:5500/dessert.html")
+}
+
+
+function mydinnercombo() {
+  location.replace("http://127.0.0.1:5500/dinnercombo.html")
+}
+
+
+function mysushi() {
+  location.replace("http://127.0.0.1:5500/sushi.html")
+}
